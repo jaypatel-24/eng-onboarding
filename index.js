955f133f7fafdc76abc0dab9c406000d2b769556
Jay Patel <friend.jay24@gmail.com>
2021-04-06 20:57:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//this code prints the total terms in fibonacci series
+// example say : input_number = 5,  output: 0 1 1 2 3
+
+const input_number  = parseInt(prompt('Enter the number: '));
+
+let n1 = 0, n2 = 1, sum = 0 ;
+let terms = 0;
+while(terms < input_number) {
+	console.log(sum+" ")
+
+	n1 = n2;
+	n2 = sum;
+    sum = n1 + n2;
+
+    term++;
+}
